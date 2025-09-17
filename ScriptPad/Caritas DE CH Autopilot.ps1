6040372c08ce284bfadc54c5664f00aeb53ddc91
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2025-09-17 08:53:22 +0200

    Update Caritas DE CH Autopilot.ps1

--- ScriptPad/Caritas DE CH Autopilot.ps1
+++ ScriptPad/Caritas DE CH Autopilot.ps1
@@ -276,9 +276,9 @@ Write-DarkGrayHost "Downloading Scripts for OOBE and specialize phase"
 
 Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/Autopilot.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\autopilot.ps1' -Encoding ascii -Force
 Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/OOBE.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.ps1' -Encoding ascii -Force
+Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/AP-Prereq.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\AP-Prereq.ps1' -Encoding ascii -Force
+Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/start-autopilotoobe.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\start-autopilotoobe.ps1 ' -Encoding ascii -Force
 Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/CleanUp.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
-Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/AP-Prereq.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
-Invoke-RestMethod https://raw.githubusercontent.com/oneictag/OSDPad/refs/heads/main/start-autopilotoobe.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
 #Invoke-RestMethod http://osdgather.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\osdgather.ps1' -Encoding ascii -Force
 
 $OOBEcmdTasks = @'
