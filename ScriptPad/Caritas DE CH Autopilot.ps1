d7fa1fbd1aeb784378d4ba3462a5ce7e6cf0f5f8
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2025-09-17 08:54:55 +0200

    Update Caritas DE CH Autopilot.ps1

--- ScriptPad/Caritas DE CH Autopilot.ps1
+++ ScriptPad/Caritas DE CH Autopilot.ps1
@@ -294,7 +294,7 @@ REM Execute OOBE Tasks
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\AP-Prereq.ps1
 
 REM Execute OOBE Tasks
-REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\start-autopilotoobe.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\start-autopilotoobe.ps1
 
 REM Execute OSD Gather Script
 REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\osdgather.ps1
